c20134bd54d418a85b2297793cd57d6cae2901ae
dntinfotech12345 <dnt.katalon@gmail.com>
2024-10-21 14:35:33 +0530

    my tests update

--- config/cucumber.js
+++ config/cucumber.js
@@ -1,18 +1,24 @@
 module.exports = {
-    default: {
+    //default is to override the cucumber default features like snippites
+    default: { 
         tags: process.env.npm_config_TAGS || "",
+        //to get the snippet option in the formate of async and await
         formatOptions: {
             snippetInterface: "async-await"
         },
+        //we have give path to locate our fetaure 
         paths: [
-            "src/test/features/doglus.feature"
+            "src/test/features/doglus.feature" 
         ],
-        publishQuiet: true,
+        //to desable the cloud default report of cucumber
+        publishQuiet: true, 
         dryRun: false,
+        //here we have kept out steps and hooks file path
         require: [
             "src/test/steps/*.ts",
             "src/hooks/hooks.ts"
         ],
+        //this required to avoid the errors for the imports statements for ts
         requireModule: [
             "ts-node/register"
         ],
